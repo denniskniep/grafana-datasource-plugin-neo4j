2598a53178fc50c27aae7b4b3e8fb6d00c2e0ad4
Dennis Kniep <kniepdennis@gmail.com>
2021-07-10 01:06:02 +0200

    Query Neo4J

--- neo4j-datasource-plugin/src/datasource.ts
+++ neo4j-datasource-plugin/src/datasource.ts
@@ -4,35 +4,33 @@ import {
   DataSourceApi,
   DataSourceInstanceSettings,
   MutableDataFrame,
-  FieldType,
+  FieldType
 } from '@grafana/data';
 
+import neo4j from 'neo4j-driver'
 import { MyQuery, MyDataSourceOptions } from './types';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+
+  dataSourceOptions: MyDataSourceOptions;
+
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+    this.dataSourceOptions = instanceSettings.jsonData;    
   }
 
-  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    const { range } = options;
-    const from = range!.from.valueOf();
-    const to = range!.to.valueOf();
+  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {    
+    //const { range } = options;    
+    //const from = range!.from.valueOf();
+    //const to = range!.to.valueOf();
 
-    // Return a constant for each query.
-    const data = options.targets.map((target) => {
-      const query = target;
-
-      return new MutableDataFrame({
-        refId: query.refId,
-        fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          //{ name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
-        ],
-      });
-    });
-
-    return { data };
+    let data: MutableDataFrame[] = []
+    for (const element in options.targets) {
+        const query = options.targets[element];        
+        let dataFrame = await cypherQuery(query, this.dataSourceOptions);        
+        data.push(dataFrame);
+    }
+    return { data };       
   }
 
   async testDatasource() {
@@ -42,4 +40,42 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       message: 'Success',
     };
   }
+}
+
+async function  cypherQuery(query : MyQuery, dataSourceOptions: MyDataSourceOptions): Promise<MutableDataFrame>  {
+  const driver = neo4j.driver(dataSourceOptions.url)
+  const session = driver.session()
+  
+  let result;
+  try {
+    result = await session.run(query.cypherQuery)
+
+    console.log(result)
+  } finally {
+    await session.close()
+  }
+
+  // on application exit:
+  await driver.close()
+
+  let dataFrame = new MutableDataFrame({
+    refId: query.refId,
+    fields: []
+  });
+  console.log("------")
+  for (const columnName of result.records[0].keys) { 
+    console.log(columnName.toString())
+    dataFrame.addField({ name: columnName.toString(), type: FieldType.string })     
+  }  
+  console.log("------")
+  for (const record of result.records) {     
+    let row : any[] = [];
+    record.map((value, entries, key) => {
+      console.log(value,entries, key )
+      row.push(value);
+    });
+    dataFrame.appendRow(row)   
+  }
+  console.log("------")
+  return dataFrame;
 }
