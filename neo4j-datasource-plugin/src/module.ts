4675648dc2a4e8b7e89c177bfb296627b8289e4c
Dennis Kniep <kniepdennis@gmail.com>
2022-01-22 22:15:22 +0100

    Queries can now be executed in variables

--- neo4j-datasource-plugin/src/module.ts
+++ neo4j-datasource-plugin/src/module.ts
@@ -3,7 +3,9 @@ import { DataSource } from './datasource';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
 import { MyQuery, MyDataSourceOptions } from './types';
+import { VariableQueryEditor } from './VariableQueryEditor';
 
 export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
+  .setVariableQueryEditor(VariableQueryEditor)
   .setQueryEditor(QueryEditor);
